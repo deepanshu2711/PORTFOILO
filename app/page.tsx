854031df2689e7e1a68d476d28defab14ab0f7b6
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-30 14:20:43 +0530

    added bottom blue

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import AboutSection from "@/components/AboutSection";
+import BlurBottom from "@/components/BlurBottom";
 import EducationSection from "@/components/EducationSection";
 import HeroSection from "@/components/HeroSection";
 import WorkExperienceSection from "@/components/WorkExperienceSection";
@@ -7,6 +8,7 @@ export default function Home() {
   return (
     <main className="max-w-3xl mx-auto px-[20px] md:px-0">
       <div className="flex flex-col gap-10 mt-16">
+        <BlurBottom />
         <HeroSection />
         <AboutSection />
         <WorkExperienceSection />
---
+++ components/BlurBottom.tsx
@@ -0,0 +1,7 @@
+const BlurBottom = () => {
+  return (
+    <div className="fixed -bottom-2 left-0 h-[58px] shadow-2xl blur-lg  bg-white  w-full  text-black" />
+  );
+};
+
+export default BlurBottom;
