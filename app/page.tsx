75176a3e048bc1029d615f9870211a62f02c3063
Deepanshu saini <deepanshusaini2711@gmail.com>
2024-08-30 15:34:18 +0530

    added skills section

--- app/page.tsx
+++ app/page.tsx
@@ -2,17 +2,19 @@ import AboutSection from "@/components/AboutSection";
 import BlurBottom from "@/components/BlurBottom";
 import EducationSection from "@/components/EducationSection";
 import HeroSection from "@/components/HeroSection";
+import SkillsSection from "@/components/SkillsSection";
 import WorkExperienceSection from "@/components/WorkExperienceSection";
 
 export default function Home() {
   return (
-    <main className="max-w-3xl mx-auto px-[20px] md:px-0">
+    <main className="max-w-3xl mx-auto px-[20px] md:px-0 mb-[60px]">
       <div className="flex flex-col gap-10 mt-16">
         <BlurBottom />
         <HeroSection />
         <AboutSection />
         <WorkExperienceSection />
         <EducationSection />
+        <SkillsSection />
       </div>
     </main>
   );
---
+++ components/SkillsSection.tsx
@@ -0,0 +1,30 @@
+import { Badge } from "./ui/badge";
+
+const SkillsSection = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <p className="text-xl font-bold">Skills</p>
+      <div className="flex flex-wrap items-center gap-2">
+        <Badge>Javascript</Badge>
+        <Badge>Typescript</Badge>
+        <Badge>Java</Badge>
+        <Badge>React</Badge>
+        <Badge>Redux</Badge>
+        <Badge>Tailwind</Badge>
+        <Badge>Next.js</Badge>
+        <Badge>Node.js</Badge>
+        <Badge>Express.js</Badge>
+        <Badge>MongoDB</Badge>
+        <Badge>PostgreSQL</Badge>
+        <Badge>Prisma</Badge>
+        <Badge>Docker</Badge>
+        <Badge>WebSockets</Badge>
+        <Badge>WebRtc</Badge>
+        <Badge>Git</Badge>
+        <Badge>GitHub</Badge>
+      </div>
+    </div>
+  );
+};
+
+export default SkillsSection;
